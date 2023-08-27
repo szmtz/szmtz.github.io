df695bb8dfa5d8e46944eb44437194ecc2e4c260
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-08-27 22:01:38 +0000

    Add Code Samples to JS

--- fem-wbd-course/js/experiments.js
+++ fem-wbd-course/js/experiments.js
@@ -2,3 +2,15 @@ const monthlyRent = 500;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
+
+/* concatenate strings old way    */
+const firstName = "Brian";
+const lastName = "Holt";
+
+/* concatenate strings new way  uses backticks for template strings  */
+const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
+

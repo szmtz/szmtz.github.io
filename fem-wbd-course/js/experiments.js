18b19f2e7eab55377daf59d9ac32bff51aa7f314
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-08-29 01:27:37 +0000

    Add JS scripts to Experiments

--- fem-wbd-course/js/experiments.js
+++ fem-wbd-course/js/experiments.js
@@ -14,3 +14,15 @@ const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
 console.log(sentence);
 console.log(sentenceWithTemplate);
 
+/* Loops */
+
+const friendsAtYourParty = 10;
+
+if (friendsAtYourParty === 0) {
+  console.log("Cool, now I have a lot of nachos to myself.");
+} else if (friendsAtYourParty <= 4) {
+  console.log("Perfect amount to play some Mario Kart.");
+} else {
+  console.log("Wooooo turn on the dance music!");
+
+  
